64260250fe2dc23b958bee0b61bc4ab00ede49be
Ubuntu <n.grosch@gmx.de>
2015-08-08 16:20:56 +0200

    Shapes in der Scene sind Shared Pointer

--- framework/scene.hpp
+++ framework/scene.hpp
@@ -11,13 +11,14 @@
 #include "renderer.hpp"
 #include <map>
 #include <vector>
+#include <memory>
 
 struct Scene
 {
 	Scene(): ambient(), cam(), render(0.0, 0.0, ""){};
 	Scene(Color const& ambient_color, Camera const& camera, Renderer const& renderer): ambient(ambient_color), cam(camera), render(renderer){};
 	std::map<std::string, Material> material;
-	std::vector<Shape> shapes; // shared pointer
+	std::vector<shared_ptr Shape> shapes; 
 	std::vector<Light_source> lights;
 	Color ambient;
 	Camera cam;
