cc9df66f8ad28c3d664aa0304b020ce280a3af92
Ubuntu <n.grosch@gmx.de>
2015-08-10 15:42:37 +0200

    Optional_hit Struct hinzugefügt

---
+++ framework/Optional_hit.hpp
@@ -0,0 +1,15 @@
+#ifndef BUW_OPTIONAL_HIT_HPP
+#define BUW_OPTIONAL_HIT_HPP 
+
+struct Optional_hit{
+	
+	Optional_hit() : hit{0}, Strahlparam{0.0f}{} 
+
+	bool hit;
+	Shape* shape;
+	float Strahlparam; //Oberflächenpunkt
+
+
+};
+
+#endif
