882e80ef797a4e4a135c9ac6c2b57091a0227f44
Ubuntu <n.grosch@gmx.de>
2015-08-27 14:37:02 +0200

    Updatet generate Rays mit Matrix aus der Vorlesung

--- framework/camera.cpp
+++ framework/camera.cpp
@@ -4,13 +4,20 @@
 #include <vector>
 #include "ray.hpp"
 #include "pixel.hpp"
+#define GLM_FORCE_RADIANS// rotate,..(functions) does not take degrees
 #include <glm/vec3.hpp>
 #include <glm/glm.hpp>
 
-Camera::Camera(std::string name, float fov_x): name_{name}, fov_x_{fov_x}, eye_{0.0,0.0,0.0}, dir_{0.0,0.0,-1}, up_{1,0.0,0.0}{}
+#include <glm/vec4.hpp> // glm::vec4
+#include <glm/mat4x4.hpp> // glm::mat4
+#include <glm/gtc/matrix_transform.hpp> // glm::translate, glm::rotate, glm::scale, glm::perspective
+
+
+
+Camera::Camera(std::string name, float fov_x): name_{name}, fov_x_{fov_x}, eye_{0.0,0.0,0.0}, dir_{0.0,0.0,-1}, up_{0.0,1,0.0}{}
 Camera::Camera(std::string name, float fov_x, glm::vec3 const& position, glm::vec3 const& direction, glm::vec3 const& up):
 name_{name},fov_x_{fov_x}, eye_{position}, dir_{direction}, up_{up}	{}
-Camera::Camera(): name_{""}, fov_x_{0.0f}, eye_{0.0,0.0,0.0}, dir_{0.0,0.0,-1}, up_{1,0.0,0.0}{}
+Camera::Camera(): name_{""}, fov_x_{0.0f}, eye_{0.0,0.0,0.0}, dir_{0.0,0.0,-1}, up_{0.0,1,0.0}{}
 Camera::~Camera(){}
 
 std::string Camera::get_name() const{
@@ -38,6 +45,7 @@ Camera& Camera::operator= (Camera const& rhs){
 	return *this;
 }
 
+/*
 void Camera::generate_rays(unsigned width, unsigned height, std::vector<Ray>& rays){
 	float distance = ((width/2) / tan(fov_x_ * M_PI / 360)); 
 	dir_ = glm::normalize(dir_);
@@ -93,6 +101,46 @@ void Camera::generate_rays(unsigned width, unsigned height, std::vector<Ray>& ra
 			//direction wird um Position der Camera verschoben
 			direction += eye_;
 			Ray temp_ray(eye_, direction);
+			rays.push_back(temp_ray);
+		}
+	}
+}*/
+
+void Camera::generate_rays(unsigned width, unsigned height, std::vector<Ray> & rays){
+	float distance = ((width/2) / tan(fov_x_ * M_PI / 360)); 
+	glm::vec3 origin(0,0,0);
+
+	for (int y = ((int)height/-2); y < (int)height/2; ++y)
+	{
+		for (int x = ((int)width/-2); x < (int)width/2; ++x)
+		{
+			glm::vec3 direction(x, y, -(distance));
+			
+			//Verrechnung mit der Matrix zur Kameraposition wenn nicht default
+			if (eye_.x != 0 || eye_.y != 0 || eye_.z != 0 ||
+				dir_.x != 0 || dir_.y != 0 || dir_.z != -1 ||
+				up_.x != 0 || up_.y != 1 || up_.z != 0 )
+			{
+				glm::vec4 direct_temp(x, y, -(distance), 1);
+				glm::vec3 u = glm::normalize(glm::cross(dir_, up_));
+				glm::vec3 v = glm::normalize(glm::cross(u, dir_));
+				dir_ = glm::normalize(dir_);
+				//Matrix wird wird Spalte x Zeile implementiert
+				glm::mat4 matrix(u.x, u.y, u.z, 0,
+								 v.x, v.y, v.z, 0,
+								 (dir_.x*-1), (dir_.y*-1), (dir_.z*-1),0,
+								 eye_.x, eye_.y, eye_.z, 1);
+				//Matrix x Vector
+				glm::vec4 new_direct = matrix * direct_temp;
+
+				direction.x = new_direct.x;
+				direction.y = new_direct.y;
+				direction.z = new_direct.z;
+			}
+
+
+
+			Ray temp_ray(origin, direction);
 			rays.push_back(temp_ray);
 		}
 	}
