dd538f893f6730f3f7e60a6396db47fa0c26bcc0
nekromantie <pia-fichtl@web.de>
2015-08-30 14:20:35 +0200

    Methode zum Lösen von Quadratgleichungen mittels abc-Formel

--- framework/shape.hpp
+++ framework/shape.hpp
@@ -40,6 +40,28 @@ public:
 	/*Material const& get_material() const{
 		return material_;
 	}*/
+	
+	bool solveQuadratic(const float &a, const float &b, const float &c, float &x0, float &x1){ //abc Formel, x0, x1 geben t0,t1 Auftreffpunkte
+		float discr = b * b - 4 * a * c;
+		if (discr < 0){
+			return false;
+		}
+		else if (discr == 0) {
+		x0 = - 0.5 * b / a;
+		x1 = - 0.5 * b / a;
+		}
+		else {
+			float q = (b > 0) ?
+				-0.5 * (b + sqrt(discr)) :
+				-0.5 * (b - sqrt(discr));
+			x0 = q / a;
+			x1 = c / q;
+		}
+		if (x0 > x1) {
+			std::swap(x0, x1);
+		}
+		return true;
+	}
 
 	friend std::ostream& operator<<(std::ostream& os, Shape const& s){
 		return s.print(os);
