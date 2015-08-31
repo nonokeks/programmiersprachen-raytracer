24b8936b33e7f7c06201f1dadc4d17821c1accaf
nekromantie <pia-fichtl@web.de>
2015-08-31 11:16:46 +0200

    cone konstruktoren...

--- framework/cone.cpp
+++ framework/cone.cpp
@@ -10,15 +10,15 @@
 #include <glm/glm.hpp>
 #include <glm/gtx/intersect.hpp>
 
-Cone::Cone(): Shape{}, center_{0.0f, 0.0f, 0.0f}, radius_{0.0f}, height_{0.0f}{}
-Cone::Cone(glm::vec3 const& center, float radius, float height): Shape{}, center_{center}, radius_{radius}, height_{height}{}
-Cone::Cone(std::string name, glm::vec3 const& center, float radius, float height, std::string material) : Shape{name, material}, center_{center}, radius_{radius}, height_{height}{}
+Cone::Cone(): Shape{}, center_{0.0f, 0.0f, 0.0f}, radius_{0.0f}, center2_{0.0f, 0.0f, 0.0f}{}
+Cone::Cone(glm::vec3 const& center, float radius, glm::vec3 const& center2): Shape{}, center_{center}, radius_{radius}, center2_{center2}{}
+Cone::Cone(std::string name, glm::vec3 const& center, float radius, glm::vec3 const& center2, std::string material) : Shape{name, material}, center_{center}, radius_{radius}, center2_{center2}{}
 
-//Cone::Cone(glm::vec3 const& center, float radius, float height, std::string name, Color const& color):Shape{name, color}, center_{center}, radius_{radius}, height_{height}{}
+//Cone::Cone(glm::vec3 const& center, float radius, glm::vec3 const& center2, std::string name, Color const& color):Shape{name, color}, center_{center}, radius_{radius}, center2_{center2}{}
 //Cone::~Cone(){std::cout << "Cone Destructor" << std::endl;}
 
-/*Cone::Cone(glm::vec3 const& center, float radius, float height, std::string name, Material const& mat): Shape{name, mat}, center_{center}, radius_{radius}, height_{height}{}
-Cone::Cone(glm::vec3 const& center, float radius, float height, std::string name, std::string name_mat, Color const& ka, Color const& kd, Color const& ks, float m): Shape{name, name_mat, ka, kd, ks, m}, center_{center}, radius_{radius}, height_{height}{}*/
+/*Cone::Cone(glm::vec3 const& center, float radius, glm::vec3 const& center2, std::string name, Material const& mat): Shape{name, mat}, center_{center}, radius_{radius}, center2_{center2}{}
+Cone::Cone(glm::vec3 const& center, float radius, glm::vec3 const& center2, std::string name, std::string name_mat, Color const& ka, Color const& kd, Color const& ks, float m): Shape{name, name_mat, ka, kd, ks, m}, center_{center}, radius_{radius}, center2_{center2}{}*/
 
 
 
@@ -30,14 +30,10 @@ float const& Cone::get_radius() const {
 	return radius_;
 }
 
-float const& Cone::get_height() const {
-	return height_;
-}
-
-/*glm::vec3 const& Cone::get_center2() const {
-	//Berechnung der Spitze
+glm::vec3 const& Cone::get_center2() const {
+	return center2_;
 	
-}*/
+}
 
 bool Cone::intersect(Ray const& ray, float& distance, glm::vec3& intersection, glm::vec3& normal)const{
 	//wohin rayplane, raydisc?
